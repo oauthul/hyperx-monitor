449aad4f536480a447bd8a71ab3a0ea3f947952e
oauthul <oauthul@proton.me>
2026-02-25 22:52:50 +0000

    clean up logic code

--- src/hardware.rs
+++ src/hardware.rs
@@ -1,83 +1,77 @@
 extern crate hidapi;
+use tracing::{info};
+use hidapi::HidApi;
 use hidapi::HidDevice;
+use hidapi::HidError;
 
 const VENDOR_ID: u16 = 0x03F0;
 const PRODUCT_ID: u16 = 0x0D93;
 const USAGE_PAGE: u16 = 0xFF90;
 const BATTERY_BUF: [u8;4] = [0x06, 0xFF, 0xBB, 0x02];
-const BATTERY_LEVEL_POS: usize = 7;
-const READ_BUF_SIZE: usize = 20;
-const FAIL_BUF_SIZE: usize = 0;
-const NO_BATTERY: u8 = 0;
+const BATTERY_LEVEL_POS: usize = 7; // position 8 for buffer is the battery level
+const READ_SIZE: usize = 20; // normal buffer size for the packet
+const FAILED_SIZE: usize = 0; // when no buffer could be read, this is the output
 
 #[derive(Default)]
 pub struct HeadsetInfo {
     pub device: Option<HidDevice>,
-    pub battery_level: u8
+    pub battery_level: Option<u8>
 }
 
-
 impl HeadsetInfo {
-    pub fn get_device_handle() -> Option<HidDevice> {
-        match hidapi::HidApi::new() {
+    pub fn get_device_handle(api: &Result<HidApi, HidError>) -> Option<HidDevice> {
+        match api {
             Ok(handle) => {
-                for target in handle.device_list() 
-                {
-                    if target.product_id() == PRODUCT_ID && target.vendor_id() == VENDOR_ID && target.usage_page() == USAGE_PAGE {
-                        println!("trying to connect to usb..");
-                        let target_device = target.open_device(&handle);
-                        match target_device 
-                        {
-                            Ok(target_device) => {
-                                println!("successfully connected to usb!");
-                                return Some(target_device)
-                            },
-
-                            Err(open_err) => {
-                                println!("failed to open device! error: {}", open_err);
-                                return None
-                            }
-                        }
-                      
+                let target = handle.device_list().find(|&target| target.product_id() == PRODUCT_ID &&
+                                            target.vendor_id() == VENDOR_ID &&
+                                            target.usage_page() == USAGE_PAGE)?;
+                let device = target.open_device(&handle);
+                match device {
+                    Ok(device) => {
+                        info!("successfully connected to usb!");
+                        return Some(device)
+                    }
+                    Err(open_err) => {
+                        info!("failed to open device! error: {}", open_err);
+                        return None
                     }
                 }
-                return None
             }
             Err(init_err) => {
-                println!("failed to initialize! error: {}", init_err);
+                info!("failed to initialize! error: {}", init_err);
                 return None
             }
         }
 
     }
     
-    pub fn get_battery(&self, buf: &mut [u8; 64]) -> u8 {
+    pub fn get_battery(&self, buf: &mut [u8; 64]) -> Option<u8> {
         if let Some(target) = &self.device {
             let write_buffer = target.write(&BATTERY_BUF);
             match write_buffer {
                 Ok(bytes) => {
-                    println!("successfully wrote data to headset, written {} bytes.", bytes);
+                    info!("successfully wrote data to headset, written {} bytes.", bytes);
                 }
                 Err(write_error) => {
-                    println!("failed to write data to headset! error: {}", write_error);
-                    return NO_BATTERY
+                    info!("failed to write data to headset! error: {}", write_error);
+                    return None
                 }
             };
 
             let read_buffer = target.read(buf);
             match read_buffer {
                 Ok(read_bytes) => {
-                    if read_bytes == READ_BUF_SIZE {
-                        println!("successfully read buffer, read {} bytes.", read_bytes);
-                    } else if read_bytes == FAIL_BUF_SIZE {
-                        println!("failed to read buffer! is the device connected? read {} bytes.", read_bytes)
+                    if read_bytes == READ_SIZE {
+                        info!("successfully read buffer, read {} bytes.", read_bytes);
+                    } else if read_bytes == FAILED_SIZE {
+                        info!("failed to read buffer! is the device connected? read {} bytes.", read_bytes)
                     }
                 },
-                Err(read_err) => println!("failed to read buffer! error: {}", read_err),
+                Err(read_err) => info!("failed to read buffer! error: {}", read_err),
             }
-            return buf[BATTERY_LEVEL_POS];
+            return Some(buf[BATTERY_LEVEL_POS])
 
         }
-        return NO_BATTERY
+        return None
     } 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
-use crossbeam_channel::unbounded;
+extern crate tracing;
 mod graphics;
 mod hardware;
+
+use hidapi::HidApi;
+use tracing::{info, warn, error};
+use crossbeam_channel::unbounded;
 use hardware::HeadsetInfo;
 
 fn main() {
+    let api = HidApi::new();
     let mut response_buf = [0u8; 64];
     let mut headset = HeadsetInfo::default();
 
-    headset.device = HeadsetInfo::get_device_handle();
+    headset.device = HeadsetInfo::get_device_handle(&api);
 
     match headset.device {
-        Some(_) => println!("device handle found!"),
-        None => println!("device handle not found!")
+        Some(_) => info!("device handle found!"),
+        None => error!("device handle not found!")
     }
 
     headset.battery_level = headset.get_battery(&mut response_buf);
-    
-    if headset.battery_level > 0 {
-        println!("current battery percentage: {}%", headset.battery_level);
-    } else {
-        println!("failed to get battery level!");
+
+    match headset.battery_level {
+        Some(level) => println!("found battery level: {}%", level),
+        None => error!("failed to find battery level, is device disconnected?")
     }
+
+    headset.device = HeadsetInfo::get_device_handle(&api);
 
 }
